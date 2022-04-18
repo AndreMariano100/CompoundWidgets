1cfdb9d1d74f52b50578ac42365f18a1a0d49fc3
Andre Mariano <andremariano@petrobras.com.br>
2022-04-18 19:49:58 -0300

    Almost a package

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-from COMPOUND_WIDGETS import *
-from CUSTOM_BUTTONS import *
-from CUSTOM_FRAMES import *
-from POP_UP_WIDGETS import *
-from TOP_LEVEL_WIDGETS import *
+__version__ = "0.1"
+__author__ = 'André Mariano'
+
+from CompoundWidgets.COMPOUND_WIDGETS import *
+from CompoundWidgets.CUSTOM_FRAMES import *
+from CompoundWidgets.CUSTOM_BUTTONS import *
+from CompoundWidgets.MESSAGE_BOX_WIDGETS import *
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='CompoundWidgets',
+    version='0.1',
+    description='Compound TTK Widgets',
+    url='https://github.com/AndreMariano100/CompoundWidgets.git',
+    author='André Mariano',
+    author_email='andremariano100@gmail.com',
+    license='none',
+    packages=['CompoundWidgets'],
+    install_requires=['ttkbootstrap'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+    ],
+)
