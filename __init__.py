887a8c84fa84d0fe3969f67cd774f8e78df79d6e
Andre Mariano <andremariano@petrobras.com.br>
2022-04-19 06:52:53 -0300

    Almost a package

--- __init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = "0.1"
-__author__ = 'André Mariano'
-
-from compoundwidgets.COMPOUND_WIDGETS import *
-from compoundwidgets.CUSTOM_FRAMES import *
-from compoundwidgets.CUSTOM_BUTTONS import *
-from compoundwidgets.MESSAGE_BOX_WIDGETS import *
-
---
+++ compoundwidgets/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1"
+__author__ = 'André Mariano'
+
+from COMPOUND_WIDGETS import *
+from CUSTOM_FRAMES import *
+from CUSTOM_BUTTONS import *
+from MESSAGE_BOX_WIDGETS import *
